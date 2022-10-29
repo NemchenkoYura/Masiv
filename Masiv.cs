6f15d0dc39bc1162d3774fcb118b7437a96528af
NemchenkoYura <Zoroo3721@gmail.com>
2022-10-29 13:57:12 +0300

    Create Masiv

---
+++ Masiv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива:\t");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] b = new int[a];
+
+            for (int i = 0; i <b.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}:\t ");
+                b[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nВывод массива:");
+
+            for (int i = 0; i < b.Length;i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+        }
+    }
+}
